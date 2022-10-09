2bc710e88204756aca1a52258f692a5fc0e79eed
Andrea <90292543+amgallo1213@users.noreply.github.com>
2022-10-08 22:42:22 -0400

    deleted unnecessary react-app files

--- japango/src/App.js
+++ japango/src/App.js
@@ -1,12 +1,13 @@
 import './App.css';
 import Header from './components/Header';
+import HeroImg from './components/Hero';
+
 
 function App() {
   return (
     <div >
       <Header />
-      {/* <h1>Hello World</h1> */}
-      <img src="https://images.pexels.com/photos/8085870/pexels-photo-8085870.jpeg?auto=compress&cs=tinysrgb&w=800" alt="orange gate at night" id="mainImg"/>
+      <HeroImg />
     </div>
   );
 }
---
+++ japango/src/components/Hero.js
@@ -0,0 +1,13 @@
+
+
+
+const HeroImg = () => {
+    return ( 
+        <div>
+            <h1>JapanGo</h1>
+            <img src="https://images.pexels.com/photos/8085870/pexels-photo-8085870.jpeg?auto=compress&cs=tinysrgb&w=800" alt="orange gate at night" id="mainImg"/>
+        </div>
+    );
+}
+ 
+export default HeroImg;
